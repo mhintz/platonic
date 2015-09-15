90092399ece22d633db5488d06c8f5deae5f0b2f
Mark Hintz <markohintz@gmail.com>
2015-09-15 11:01:18 +0200

    Redo the tetrahedron calculation to be better aligned with the axes

--- src/tetrahedron.js
+++ src/tetrahedron.js
@@ -3,21 +3,27 @@ import {vec3} from 'gl-matrix';
 import * as util from './util';
 
 function getRawTetrahedron() {
-  let len = 1 / Math.sqrt(2);
+  let a0 = 2 * Math.PI * 0;
+  let a1 = 2 * Math.PI * 1 / 3;
+  let a2 = 2 * Math.PI * 2 / 3;
+  // When the circumradius of the tetrahedron is 1, the height is 4 / 3
+  let height = 4 / 3;
+  let dBack = 1 - height; // Distance to the 'back' of the tetrahedron
+  let dPoint = Math.sqrt(0.5) * height;
 
-  let v1 = vec3.fromValues(1, 0, -len);
-  let v2 = vec3.fromValues(-1, 0, -len);
-  let v3 = vec3.fromValues(0, 1, len);
-  let v4 = vec3.fromValues(0, -1, len);
+  let v1 = vec3.fromValues(0, 0, 1);
+  let v2 = vec3.fromValues(dPoint * Math.cos(a0), dPoint * Math.sin(a0), dBack);
+  let v3 = vec3.fromValues(dPoint * Math.cos(a1), dPoint * Math.sin(a1), dBack);
+  let v4 = vec3.fromValues(dPoint * Math.cos(a2), dPoint * Math.sin(a2), dBack);
 
   let vertices = [ v1, v2, v3, v4 ];
 
   let triangles = [];
 
-  triangles.push([0, 2, 3]);
-  triangles.push([3, 2, 1]);
-  triangles.push([1, 0, 3]);
-  triangles.push([2, 0, 1]);
+  triangles.push([0, 1, 2]);
+  triangles.push([2, 3, 0]);
+  triangles.push([3, 1, 0]);
+  triangles.push([1, 3, 2]);
 
   return [vertices, triangles];
 }
