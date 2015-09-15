049f8431f077639892228bf01c3bda7e6785b69b
Mark Hintz <markohintz@gmail.com>
2015-09-15 15:54:21 +0200

    Adjust the way the cube vertices are calculated to align one of its points with the z-axis

--- src/cube.js
+++ src/cube.js
@@ -1,46 +1,59 @@
-import {vec3} from 'gl-matrix';
+import {vec3, quat} from 'gl-matrix';
 
 import * as util from './util';
 
 function getRawCube() {
-  let len = 1 / Math.sqrt(3);
+  let irad = Math.sqrt(1 / 3);
+  let diag = Math.sqrt(2 / 3);
 
-  let v1 = vec3.fromValues( len,  len,  len);
-  let v2 = vec3.fromValues(-len,  len,  len);
-  let v3 = vec3.fromValues(-len, -len,  len);
-  let v4 = vec3.fromValues( len, -len,  len);
-  let v5 = vec3.fromValues( len, -len, -len);
-  let v6 = vec3.fromValues( len,  len, -len);
-  let v7 = vec3.fromValues(-len,  len, -len);
-  let v8 = vec3.fromValues(-len, -len, -len);
+  // Rotates the cube around the x axis to align a point with the z axis
+  let rotation = quat.create();
+  quat.rotateX(rotation, rotation, Math.PI / 4);
+
+  let v1 = vec3.fromValues(0, irad, diag);
+  vec3.transformQuat(v1, v1, rotation);
+  let v2 = vec3.fromValues(diag, irad, 0);
+  vec3.transformQuat(v2, v2, rotation);
+  let v3 = vec3.fromValues(0, irad, -diag);
+  vec3.transformQuat(v3, v3, rotation);
+  let v4 = vec3.fromValues(-diag, irad, 0);
+  vec3.transformQuat(v4, v4, rotation);
+  let v5 = vec3.fromValues(-diag, -irad, 0);
+  vec3.transformQuat(v5, v5, rotation);
+  let v6 = vec3.fromValues(0, -irad, diag);
+  vec3.transformQuat(v6, v6, rotation);
+  let v7 = vec3.fromValues(diag, -irad, 0);
+  vec3.transformQuat(v7, v7, rotation);
+  let v8 = vec3.fromValues(0, -irad, -diag);
+  vec3.transformQuat(v8, v8, rotation);
 
   let vertices = [ v1, v2, v3, v4, v5, v6, v7, v8 ];
 
   let triangles = [];
 
-  // Front
+  // front top
   triangles.push([0, 1, 2]);
   triangles.push([0, 2, 3]);
 
-  // Right
-  triangles.push([5, 0, 3]);
-  triangles.push([5, 3, 4]);
+  // front bottom right
+  triangles.push([0, 6, 1]);
+  triangles.push([0, 5, 6]);
 
-  // Bottom
-  triangles.push([3, 2, 7]);
-  triangles.push([3, 7, 4]);
+  // front bottom left
+  triangles.push([0, 3, 4]);
+  triangles.push([0, 4, 5]);
 
-  // Left
-  triangles.push([1, 6, 7]);
-  triangles.push([1, 7, 2]);
+  // back top right
+  triangles.push([7, 2, 1]);
+  triangles.push([7, 1, 6]);
 
-  // Back
-  triangles.push([6, 5, 4]);
-  triangles.push([6, 4, 7]);
+  // back bottom
+  triangles.push([7, 6, 5]);
+  triangles.push([7, 5, 4]);
 
-  // Top
-  triangles.push([5, 6, 1]);
-  triangles.push([5, 1, 0]);
+  // back top left
+  triangles.push([7, 4, 3]);
+  triangles.push([7, 3, 2]);
 
   return [vertices, triangles];
 }
